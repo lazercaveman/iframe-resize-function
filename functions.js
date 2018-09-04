b3d7836bf0405c83c08f706c13155d9b0c389af7
Ali Soueidan <a.soueidan@live.de>
2018-09-04 17:18:06 +0200

    Adjusting indent

--- functions.js
+++ functions.js
@@ -5,16 +5,16 @@
   // ThisFunction will resize iframe height value related to the iframes width
   function setIframeSize () {
 
-      var iframeWidth = document.querySelector('iframe').offsetWidth;
+    var iframeWidth = document.querySelector('iframe').offsetWidth;
 
-      var iframeAdjustment = iframeWidth / 1.778;
+    var iframeAdjustment = iframeWidth / 1.778;
 
-      document.querySelector('iframe').style.height = iframeAdjustment + "px";
+    document.querySelector('iframe').style.height = iframeAdjustment + "px";
 
-    };
+  };
 
-    // Execute function on load
-    setIframeSize();
+  // Execute function on load
+  setIframeSize();
 
-    // Execute function on window resize
-    window.addEventListener("resize", setIframeSize);
+  // Execute function on window resize
+  window.addEventListener("resize", setIframeSize);
